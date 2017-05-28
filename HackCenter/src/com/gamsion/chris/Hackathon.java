81c90876467831416054408293ca93de935b01b9
gamma2653 <Christopher2653@gmail.com>
2017-05-28 06:16:46 -0400

    Added Events and constructors

--- HackCenter/src/com/gamsion/chris/Hackathon.java
+++ HackCenter/src/com/gamsion/chris/Hackathon.java
@@ -1,6 +1,8 @@
 package com.gamsion.chris;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Hackathon {
@@ -8,14 +10,26 @@ public class Hackathon {
 	String description;
 	Map<String, Map<String, String>> organizers = new HashMap<String, Map<String, String>>();
 	Map<String, Map<String, String>> students =  new HashMap<String, Map<String, String>>();
-	Map<String, Map<String, String>> events = new HashMap<String, Map<String, String>>();
+	List<Event> events = new ArrayList<Event>();
 
-	public Hackathon(String name, String description, Map<String, Map<String, String>> organizers, Map<String, Map<String, String>> students, Map<String, Map<String, String>> events){
+	public Hackathon(String name, String description, Map<String, Map<String, String>> organizers, Map<String, Map<String, String>> students, List<Event> events){
 		this.name = name;
 		this.description = description;
 		this.organizers = organizers;
 		this.students = students;
 		this.events = events;
+	}
+	public class Event{
+		
+		String name;
+		String description;
+		String time;
+		
+		public Event(String name, String description, String time){
+			this.name = name;
+			this.description = description;
+			this.time = time;
+		}
 	}
 
 }
