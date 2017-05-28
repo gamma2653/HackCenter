bac878fb85f0812eee449c70ad1ad8488b36352c
Christopher De Jesus <Christopher2653@gmail.com>
2017-05-28 02:25:59 -0400

    added stuffs

---
+++ HackCenter/src/com/gamsion/chris/Hackathon.java
@@ -0,0 +1,9 @@
+package com.gamsion.chris;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hackathon {
+	String name;
+	Map<String, Map<String, String>> organizers = new HashMap<String, Map<String, String>>();
+}
