322d7e3a2ecbec9d244809591f6f0652a8eabab0
gamma2653 <Christopher2653@gmail.com>
2017-05-28 00:46:09 -0400

    Added Server

--- HackCenter/src/com/gamsion/chris/Server.java
+++ HackCenter/src/com/gamsion/chris/Server.java
@@ -1,7 +1,25 @@
 package com.gamsion.chris;
-import static spark.Spark.*;
+import static spark.Spark.post;
+
+import java.util.HashMap;
+import java.util.Map;
 public class Server {
 	public static void main(String[] args) {
-		get("/hello", (req, res) -> "Hello World");
+		post("/login", (req, res) -> {
+			System.out.println(req.body());
+			System.out.println(stringToMap(req.body()));
+			return "";
+		});
+	}
+	private static Map<String, String> stringToMap(String in){
+		Map<String, String> map = new HashMap<String, String>();
+		for(String s: in.split("&")){
+			String[] pair = s.split("=");
+			if(pair.length!=2){
+				throw new RuntimeException("There is not a pair.");
+			}
+			map.put(pair[0], pair[1]);
+		}
+		return map;
 	}
 }
